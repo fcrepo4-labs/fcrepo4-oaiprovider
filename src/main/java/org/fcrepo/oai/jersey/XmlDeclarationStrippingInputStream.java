5e675e314769d2163de8db8edbe42057ec9e51b3
Nianli Ma <nianma@indiana.edu>
2015-02-12 13:48:15 -0500

    add description for @param, remove HTML tag for code, replace by @code

--- src/main/java/org/fcrepo/oai/jersey/XmlDeclarationStrippingInputStream.java
+++ src/main/java/org/fcrepo/oai/jersey/XmlDeclarationStrippingInputStream.java
@@ -21,66 +21,66 @@ import java.io.InputStream;
 /**
  * An {@link InputStream} implementation which strips the leading XML
  * Declaration of a XML document If the xml document starts with
- * <code><?xml...?></code>, the implementation will skip these bytes and start
+ * {@code<?xml...?>}, the implementation will skip these bytes and start
  * streaming directly after the XML declaration
  *
  * @author frank asseg
  */
 public class XmlDeclarationStrippingInputStream extends InputStream {
 
-    private final InputStream src;
+	private final InputStream src;
 
-    private String firstElement;
+	private String firstElement;
 
-    private boolean checked = false;
+	private boolean checked = false;
 
-    private boolean hasDeclaration = false;
+	private boolean hasDeclaration = false;
 
-    private int elementIndex;
+	private int elementIndex;
 
-    /**
-     * Instantiates a new Xml declaration stripping input stream.
-     *
-     * @param src the src
-     */
-    public XmlDeclarationStrippingInputStream(final InputStream src) {
-        super();
-        this.src = src;
-    }
+	/**
+	 * Instantiates a new Xml declaration stripping input stream.
+	 *
+	 * @param src the src
+	 */
+	public XmlDeclarationStrippingInputStream(final InputStream src) {
+		super();
+		this.src = src;
+	}
 
-    @Override
-    public int read() throws IOException {
-        if (!checked) {
-            checked = true;
-            final StringBuffer name = new StringBuffer();
-            int b = src.read();
-            if (b == -1) {
-                return -1;
-            }
-            while (Character.isWhitespace(b) || Character.isISOControl(b)) {
-                b = src.read();
-            }
-            if ((char) b == '<') {
-                name.append((char) b);
-                while ((b = src.read()) != -1 && (char) b != '>') {
-                    name.append((char) b);
-                }
-                name.append((char) b);
-                firstElement = name.toString();
-                if (firstElement.toLowerCase().startsWith("<?xml ")) {
-                    hasDeclaration = true;
-                    b = src.read();
-                    while (Character.isWhitespace(b) || Character.isISOControl(b)) {
-                        b = src.read();
-                    }
-                    return b;
-                }
-            }
-        }
-        if (!hasDeclaration && elementIndex < firstElement.length()) {
-            return firstElement.charAt(elementIndex++);
-        }
-        return src.read();
-    }
+	@Override
+	public int read() throws IOException {
+		if (!checked) {
+			checked = true;
+			final StringBuffer name = new StringBuffer();
+			int b = src.read();
+			if (b == -1) {
+				return -1;
+			}
+			while (Character.isWhitespace(b) || Character.isISOControl(b)) {
+				b = src.read();
+			}
+			if ((char) b == '<') {
+				name.append((char) b);
+				while ((b = src.read()) != -1 && (char) b != '>') {
+					name.append((char) b);
+				}
+				name.append((char) b);
+				firstElement = name.toString();
+				if (firstElement.toLowerCase().startsWith("<?xml ")) {
+					hasDeclaration = true;
+					b = src.read();
+					while (Character.isWhitespace(b) || Character.isISOControl(b)) {
+						b = src.read();
+					}
+					return b;
+				}
+			}
+		}
+		if (!hasDeclaration && elementIndex < firstElement.length()) {
+			return firstElement.charAt(elementIndex++);
+		}
+		return src.read();
+	}
 
 }
